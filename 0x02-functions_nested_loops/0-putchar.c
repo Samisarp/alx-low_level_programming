0dec03f0ee526cae5e1c66b1893e4a1eef12b733
Samisarp <sarpongs181@gmail.com>
2023-03-21 21:44:33 -0700

    Solution to task 0 added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,15 @@
-#include  <stdio.h> 
-int main(void){
-	printf("_putchar\n");
+#include "main.h" 
+int main(void)
+{
+	char myVar[] = "_putchar";
+	int i = 0;
+
+	while (myVar[i] != '\0')
+	{
+		_putchar(myVar[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
