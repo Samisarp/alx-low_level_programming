fcfa8770a4816a93110671174ddd42fc93696ee8
Samisarp <sarpongs181@gmail.com>
2023-04-03 11:04:36 -0700

    Add solution to task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,12 @@
  * main-a function that prints the numbers, from 0 to 9, followed by a new line.
  * Return- always return 0
  */
-
-int main(void){
-	void print_most_numbers(void);
+void print_most_numbers(void){
 		int i;
 		for(i=0; i<10; i++){
 			if (!(i==2 || i==4)){
-			_putchar(i);
+				_putchar(i);
 			}
 		}	
 		_putchar('\n');
-	return (0);
 }
